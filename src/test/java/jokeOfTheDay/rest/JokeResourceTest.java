4527e8e12e481fbdfb8b7dfa6848d7966083ca8f
Mart Laus <mart.laus@ttu.ee>
2015-10-10 15:07:40 +0300

    LIISARAUD-10 lisasin nalja ressursi (rest api) testi ja parandasin unit testide testdata

--- src/test/java/jokeOfTheDay/rest/JokeResourceTest.java
+++ src/test/java/jokeOfTheDay/rest/JokeResourceTest.java
@@ -1,7 +1,42 @@
 package jokeOfTheDay.rest;
 
+import jokeOfTheDay.common.test.ResourceIntegrationTestBase;
+import jokeOfTheDay.model.Joke;
+import org.junit.Test;
+
+import javax.ws.rs.core.GenericType;
+import javax.ws.rs.core.Response;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
 /**
  * Created by mart on 28.09.15.
  */
-public class JokeResourceTest {
+public class JokeResourceTest extends ResourceIntegrationTestBase {
+
+    @Test
+    public void getAllJokes() {
+        Response response = doGet("joke");
+
+        List<Joke> jokes = response.readEntity(new GenericType<List<Joke>>() {
+        });
+
+        assertEquals(2, jokes.size());
+        assertValidJoke(jokes.get(0));
+        assertValidJoke(jokes.get(1));
+
+    }
+
+    private void assertValidJoke(Joke joke) {
+        assertNotNull(joke.getId());
+        assertNotNull(joke.getJoke());
+        if (joke.getId() == 1) {
+            assertEquals("yo moma so fat", joke.getJoke());
+        } else if (joke.getId() == 2) {
+            assertEquals("yo papa so fat", joke.getJoke());
+        } else {
+            fail("Joke with unexpected id.");
+        }
+    }
 }
--- src/test/resources/import.sql
+++ src/test/resources/import.sql
@@ -1,2 +1,2 @@
-insert into Joke(id, joke) values (1, 'yo moma so fat');
-insert into Joke(id, joke) values (2, 'yo papa so fat');
+INSERT INTO Joke (id, joke, added) VALUES (1, 'yo moma so fat', '1999-02-02 06:00:01');
+INSERT INTO Joke (id, joke, added) VALUES (2, 'yo papa so fat', '1999-02-02 06:00:01');
